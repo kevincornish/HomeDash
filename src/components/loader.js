b56e0313c43338d05c391a03923b4242dc3919b5
Kevin Cornish <kevinz_c@live.com>
2022-01-23 20:00:01 +0000

    add loader component

---
+++ src/components/loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import loader from "../images/loader.gif";
+
+const Loader = () => {
+  return (
+    <img
+      src={loader}
+      style={{
+        width: "20px",
+        margin: "auto",
+        display: "block",
+        alt: "Loading",
+      }}
+    />
+  );
+};
+
+export default Loader;
