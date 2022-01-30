c05c634191210cbae3f7e3657bdee9969e5b7a81
Kevin Cornish <kevinz_c@live.com>
2022-01-30 10:56:30 +0000

    fix footer disable

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,6 @@
 import { Container, Typography } from "@mui/material";
-import { FooterEnable } from "../config";
 
 function Footer() {
-  if (!FooterEnable) return null;
   return (
     <Container maxWidth="100" style={{ background: "#2E3B55" }}>
       <Typography variant="body2" color="text.footer" align="center">
